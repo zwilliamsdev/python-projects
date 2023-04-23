d05db10e44bf16a6dc19f849933c82f431298444
Zachary Williams <zwilliams.dev@gmail.com>
2023-04-22 23:44:44 -0400

    Number guessing game first draft

---
+++ number_guessing/main.py
@@ -0,0 +1,81 @@
+import random
+
+# Max number player can guess 0 to MAX
+MAX: int = 100
+
+# Difficulty Modifiers
+NORMAL: int = 15
+MEDIUM: int = 10
+HARD: int = 5
+
+
+def get_difficulty() -> int:
+    while True:
+        print("Select Difficulty:")
+        print(f"\t1 - Normal ({NORMAL})")
+        print(f"\t2 - Medium ({MEDIUM})")
+        print(f"\t3 - Hard ({HARD})")
+        difficulty = input("Select Difficulty: ")
+        try:
+            difficulty = int(difficulty)
+        except ValueError:
+            print("You must enter a difficult. (1, 2, or 3)")
+            continue
+
+        if difficulty == 1:
+            print("Selected: Normal")
+            return NORMAL
+        elif difficulty == 2:
+            print("Selected: Medium")
+            return MEDIUM
+        elif difficulty == 3:
+            print("Selected: Hard")
+            return HARD
+        else:
+            # Should never run
+            return NORMAL
+
+
+def generate_number() -> int:
+    """Return a number between 0 and max"""
+    return random.randrange(MAX)
+
+
+def guessing_loop(guesses_remaining: int, secret: int):
+    """Make player guess until out of guesses
+    or they get the correct answer."""
+    # While the player has guesses
+    while guesses_remaining > 0:
+        # Have the player input a guess and give them a hint
+        # for how high the number can go
+        guess = input(f"Guess (0 - {MAX}): ")
+        # Convert guess to int
+        try:
+            guess = int(guess)
+        except ValueError:
+            # Could not convert string to int error and start next loop
+            print(f"You must enter a whole number between 0 and {MAX}")
+            continue
+        if guess == secret:
+            print("You win! Congrats!")
+            break
+        elif guess > secret:
+            print("Too high try again!")
+        elif guess < secret:
+            print("Too low try again!")
+        else:
+            # This should never run
+            print("Incorrect! Try again!")
+
+        # Take away a guess from the player
+        guesses_remaining -= 1
+    print("Out of guesses... better luck next time!")
+
+
+if __name__ == "__main__":
+    # Get amount of guesses by difficulty
+    max_guesses = get_difficulty()
+    # Create a secret number to guess
+    secret_number = generate_number()
+    # Start main game loop
+    guessing_loop(max_guesses, secret_number)
